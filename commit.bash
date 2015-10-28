a3ace95447b4b2c7def386da933a4b6aaf16ed57
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-10-28 12:10:05 -0200

    Evitar uso de códigos de referências repetidos

--- commit.bash
+++ commit.bash
@@ -4,6 +4,11 @@ title="$(sed -ne 's/\ttitle={\(.*\)},\?$/\1/p' ref.bib | tail -n 1)"
 ref="$(sed -ne '/^--*$/{g;s/:$//;p};h' ref.txt  | tail -n 1)"
 file="$(mktemp)"
 
+if [ "$(grep "^$ref:$" ref.txt | wc -l)" -gt 1 ]; then
+    echo "Repeated reference!"
+    exit 1
+fi
+
 cat > "$file" << EOF
 Resumo da referência $ref
 
